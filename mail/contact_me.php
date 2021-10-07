ff0eb8965add0b79f33d477a32b8adfb9fe9f460
Xavier Navarro <xanasa14@hotmail.com>
2021-10-07 14:14:25 -0500

    SQL uploaded
    Updated one more cert uploaded

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -8,17 +9,18 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-	
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
+
 // Create the email and send the message
-$to = 'xfn2@njit.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'xanasa14@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+$headers .= "Reply-To: $email_address";
+//mail($to,$email_subject,$email_body,$headers);
+if(mail($to,$email_subject,$email_body,$headers)) echo "mail sent"; else echo "not send";
+return true;
 ?>
